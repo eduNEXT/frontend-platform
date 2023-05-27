7ab420c74fa50220c0386a3e21b6c4333aee86f8
Adam Stankiewicz <agstanki@gmail.com>
2023-05-27 18:38:45 -0400

    fix: expose setThemeVariant

--- src/react/AppProvider.jsx
+++ src/react/AppProvider.jsx
@@ -10,6 +10,7 @@ import {
   useAppEvent,
   useParagonTheme,
 } from './hooks';
+import { paragonThemeActions } from './reducers';
 import { getAuthenticatedUser, AUTHENTICATED_USER_CHANGED } from '../auth';
 import { getConfig } from '../config';
 import { CONFIG_CHANGED } from '../constants';
@@ -74,7 +75,9 @@ export default function AppProvider({ store, children, wrapWithRouter }) {
     locale,
     paragonTheme: {
       state: paragonThemeState,
-      dispatch: paragonThemeDispatch,
+      setThemeVariant: (themeVariant) => {
+        paragonThemeDispatch(paragonThemeActions.setParagonThemeVariant(themeVariant));
+      },
     },
   }), [authenticatedUser, config, locale, paragonThemeState, paragonThemeDispatch]);
 
