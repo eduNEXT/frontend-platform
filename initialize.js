2e914a12482565a20aa83709a752dfb5c65253ae
Jose Ignacio Palma <jignaciopm13@gmail.com>
2025-01-06 23:22:28 -0400

    chore: update dist

--- initialize.js
+++ initialize.js
@@ -51,7 +51,7 @@ function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = 
  */
 
 import { createBrowserHistory, createMemoryHistory } from 'history';
-console.debug('otra pruebaaaaaaaa');
+console.debug('testttttttttt');
 /*
 This 'env.config' package is a special 'magic' alias in our webpack configuration in frontend-build.
 It points at an `env.config.js` file in the root of an MFE's repository if it exists and falls back
